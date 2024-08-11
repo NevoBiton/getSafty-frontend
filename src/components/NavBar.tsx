38edb44153da66b4c3ed44c026d3bed79619a603
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-08-11 19:41:14 +0300

    ori - drawer

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,41 +1,49 @@
+import { useState } from "react";
 import { FaRegBookmark, FaRegUser } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { GrMapLocation } from "react-icons/gr";
 import { useNavigate } from "react-router-dom";
-
+import Drawer from "./Drawer";
 function NavBar() {
+  const [openLeft, setOpenLeft] = useState(false);
+
   const navigate = useNavigate();
+
   return (
     <>
-      <div className="bg-blue-800 flex justify-between items-center p-4 relative">
-        <div className="flex items-center gap-28 pl-14">
-          <button className="text-white hover:text-gray-300">
-            <GiHamburgerMenu size={40} />
+      <div className="bg-blue-800 flex justify-between p-4 relative">
+        <div className="w-28 sm:w-36 flex justify-between pl-3">
+          <button
+            onClick={() => setOpenLeft(!openLeft)}
+            className="text-white hover:text-gray-300"
+          >
+            <GiHamburgerMenu size={32} />
           </button>
-          <button className="text-white hover:text-gray-300">
-            <FaRegUser size={40} />
+          <button className="text-white hovser:text-gray-300">
+            <FaRegUser size={32} />
           </button>
         </div>
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 rounded-full border-4 border-black p-3 bg-white">
+        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 rounded-full border-4 border-black p-3 bg-white">
           <GrMapLocation
             onClick={() => navigate("/map")}
-            size={60}
+            size={50}
             className="text-blue-800"
           />
         </div>
-        <div className="flex items-center gap-28 pr-14">
+        <div className="w-32 flex justify-between pl-3">
           <button className="text-white hover:text-gray-300">
-            <FaRegBookmark size={40} />
+            <FaRegBookmark size={32} />
           </button>
           <button>
             <img
-              className="w-16 h-auto rounded-full "
+              className="w-12 h-auto rounded-full "
               src="../src/images/pikud-haOref-logo.png"
               alt="Pikud HaOref Logo"
             />
           </button>
         </div>
       </div>
+      <Drawer open={openLeft} setOpen={setOpenLeft} />
     </>
   );
 }
