55c2aa9239120975f5acd598ae4eadced684cdde
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-08-11 10:22:15 +0300

    ori - components & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import MapPage from "./pages/MapPage";
+import RegisterPage from "./pages/RegisterPage";
+import InstructionsPage from "./pages/InstructionsPage";
+import MyProfile from "./pages/MyProfile";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <div>hello</div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="map" element={<MapPage />} />
+        <Route path="instructions" element={<InstructionsPage />} />
+        <Route path="myProfile" element={<MyProfile />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/RoomModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RoomModal() {
+  return <div>RoomModal</div>;
+}
+
+export default RoomModal;
